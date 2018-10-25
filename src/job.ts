0f9ba8104111b4093f128e92f28af05fde61a863
Dominic Foti <dominic@fotijr.com>
2018-10-25 00:04:28 -0400

    Added integration test & job interface

---
+++ src/job.ts
@@ -0,0 +1,16 @@
+enum JobStatus{
+    Ordered,
+    Started, 
+    Complete,
+    Cancelled
+}
+
+interface Job {
+    id: string;
+    dateCreated: Date;
+    name: string;
+    status: JobStatus;
+    storedPartCount: number;
+}
+
+export { Job }
