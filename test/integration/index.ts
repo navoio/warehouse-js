99148687a4f75f60d12d75ff98632187290815b5
Dominic Foti <dominic@fotijr.com>
2019-12-16 20:25:14 -0500

    Lint fix

--- test/integration/index.ts
+++ test/integration/index.ts
@@ -1,12 +1,12 @@
-import chai from 'chai';
 import { AxiosError } from 'axios';
+import chai from 'chai';
 import { NavoClient } from '../../src';
 const expect = chai.expect;
 const apiUrl = 'https://my.navo.io/api';
 const credentials = {
     clientId: 'client',
     apiKey: 'api-key'
-}
+};
 
 describe('NavoClient authenticate()', () => {
     it('should set a token', async () => {
@@ -18,7 +18,7 @@ describe('NavoClient authenticate()', () => {
     it('should return a 400 error with invalid credentials', async () => {
         const client = new NavoClient(apiUrl);
         try {
-            await client.authenticate(credentials.clientId, '')
+            await client.authenticate(credentials.clientId, '');
         } catch (error) {
             return expect((error as AxiosError).response?.status).to.equal(400);
         }
